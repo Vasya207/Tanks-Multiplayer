c956749651797f75a1e4c7b393707b098d50f538
VasylKhymynets <khymynetsvasya@gmail.com>
2024-04-15 17:20:23 +0300

    Added InputReader SO

---
+++ Assets/Scripts/Input/InputReader.cs
@@ -0,0 +1,44 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using static Controls;
+
+namespace Input
+{
+    [CreateAssetMenu(fileName = "New Input Reader", menuName = "Input/Input Reader")]
+    public class InputReader : ScriptableObject, IPlayerActions
+    {
+        public event Action<bool> PrimaryFireEvent; 
+        public event Action<Vector2> MoveEvent;
+        
+        private Controls _controls;
+
+        private void OnEnable()
+        {
+            if (_controls == null)
+            {
+                _controls = new Controls();
+                _controls.Player.AddCallbacks(this);
+            }
+            
+            _controls.Player.Enable();
+        }
+
+        public void OnMove(InputAction.CallbackContext context)
+        {
+            MoveEvent?.Invoke(context.ReadValue<Vector2>());
+        }
+
+        public void OnPrimaryFire(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+            {
+                PrimaryFireEvent?.Invoke(true);
+            }
+            else if (context.canceled)
+            {
+                PrimaryFireEvent?.Invoke(false);
+            }
+        }
+    }
+}
