026020d83d1e61261600c7db5640adbf8f059f26
VasylKhymynets <khymynetsvasya@gmail.com>
2024-04-16 13:11:39 +0300

    Created player moving and aiming

---
+++ Assets/Scripts/Core/Player/PlayerAiming.cs
@@ -0,0 +1,34 @@
+using Input;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace Core.Player
+{
+    public class PlayerAiming : NetworkBehaviour
+    {
+        [SerializeField] private InputReader inputReader;
+        [SerializeField] private Transform turretTransform;
+
+        private Camera _mainCamera;
+        
+        public override void OnNetworkSpawn()
+        {
+            if (!IsOwner) return;
+
+            _mainCamera = Camera.main;
+        }
+
+        private void LateUpdate()
+        {
+            if (!IsOwner) return;
+
+            Vector2 aimScreenPosition = inputReader.AimPosition;
+            Vector2 aimWorldPosition = _mainCamera.ScreenToWorldPoint(aimScreenPosition);
+
+            turretTransform.up = new Vector2(
+                aimWorldPosition.x - turretTransform.position.x,
+                aimWorldPosition.y - turretTransform.position.y);
+            //turretTransform.up = (Vector2)_mainCamera.ScreenToWorldPoint(inputReader.AimPosition);
+        }
+    }
+}
---
+++ Assets/Scripts/Core/Player/PlayerMovement.cs
@@ -0,0 +1,55 @@
+using System;
+using Input;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace Core.Player
+{
+    public class PlayerMovement : NetworkBehaviour
+    {
+        [Header("References")]
+        [SerializeField] private InputReader inputReader;
+        [SerializeField] private Transform bodyTransform;
+        [SerializeField] private Rigidbody2D rigidbody2D;
+
+        [Header("Settings")] 
+        [SerializeField] private float movementSpeed = 4f;
+        [SerializeField] private float turningRate = 30f;
+
+        private Vector2 _previousMovementInput;
+        
+        public override void OnNetworkSpawn()
+        {
+            if (!IsOwner) return;
+
+            inputReader.MoveEvent += HandleMove;
+        }
+
+        public override void OnNetworkDespawn()
+        {
+            if (!IsOwner) return;
+            
+            inputReader.MoveEvent -= HandleMove;
+        }
+
+        private void Update()
+        {
+            if (!IsOwner) return;
+
+            float zRotation = _previousMovementInput.x * -turningRate * Time.deltaTime;
+            bodyTransform.Rotate(0f,0f, zRotation);
+        }
+
+        private void FixedUpdate()
+        {
+            if (!IsOwner) return;
+
+            rigidbody2D.velocity = (Vector2)bodyTransform.up * (_previousMovementInput.y * movementSpeed);
+        }
+
+        private void HandleMove(Vector2 currentMovementInput)
+        {
+            _previousMovementInput = currentMovementInput;
+        }
+    }
+}
