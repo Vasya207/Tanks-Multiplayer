2c7b8d8d552ec0fd2f94ade2b8fd6304a37ccab5
VasylKhymynets <khymynetsvasya@gmail.com>
2024-04-26 11:28:16 +0300

    Added UGS Wrapper

--- Assets/Scripts/Networking/Shared/UserData.cs
+++ Assets/Scripts/Networking/Shared/UserData.cs
@@ -3,10 +3,40 @@ using UnityEngine.Serialization;
 
 namespace Networking.Shared
 {
+    public enum Map
+    {
+        Default
+    }
+
+    public enum GameMode
+    {
+        Default
+    }
+
+    public enum GameQueue
+    {
+        Solo,
+        Team
+    }
+    
     [Serializable]
     public class UserData
     {
         public string userName;
         public string userAuthId;
+        public GameInfo userGamePreferences;
+    }
+
+    [Serializable]
+    public class GameInfo
+    {
+        public Map map;
+        public GameMode gameMode;
+        public GameQueue gameQueue;
+        
+        public string ToMultiplayQueue()
+        {
+            return "";
+        }
     }
 }
